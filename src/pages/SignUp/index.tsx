97703397f2cb1e3f760bd7fe99fc36dacf9374e7
Mário César Silva <mcgato33@hotmail.com>
2020-08-01 07:42:35 -0100

    Form

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -7,14 +8,18 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SingUp: React.FC = () => (
-  <Container>
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+  return ( 
+    <Container>
     <Background />
     
     <Content>
       <img src={logoImg} alt="GoBarber" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <h1>Faca o seu cadastro</h1>
 
         <Input name="name" icon={FiUser} placeholder="Nome" />
@@ -28,7 +33,7 @@ const SingUp: React.FC = () => (
         />
 
         <Button type="submit">Cadastrar</Button>
-      </form>
+      </Form>
 
       <a href="forget">
         <FiArrowLeft />
@@ -36,6 +41,7 @@ const SingUp: React.FC = () => (
       </a>
     </Content>
   </Container>
-);
+  );
+};
 
-export default SingUp;
+export default SignUp;
